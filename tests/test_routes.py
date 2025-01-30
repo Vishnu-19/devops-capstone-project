b2e5708fa9fd4c40649ef7f25e4c097db0031811
Vishnu-19 <vishnukoppera2001@gmail.com>
2025-01-30 10:06:48 -0500

    ci build

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -228,3 +228,5 @@ class TestAccountService(TestCase):
         response = self.client.get('/', environ_overrides=HTTPS_ENVIRON)
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         self.assertEqual(response.headers.get('Access-Control-Allow-Origin'), '*')
+
+        
