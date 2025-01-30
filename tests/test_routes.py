4b7b575bb651bcc72d9186a8a6d00fb9da922e72
Vishnu <vishnukoppera2001@gmail.com>
2025-01-30 10:30:53 -0500

    ci build

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -12,7 +12,6 @@ from tests.factories import AccountFactory
 from service.common import status  # HTTP Status Codes
 from service.models import db, Account, init_db
 from service.routes import app
-from service import talisman
 
 DATABASE_URI = os.getenv(
     "DATABASE_URI", "postgresql://postgres:postgres@localhost:5432/postgres"
@@ -39,9 +38,6 @@ class TestAccountService(TestCase):
         app.config["SQLALCHEMY_DATABASE_URI"] = DATABASE_URI
         app.logger.setLevel(logging.CRITICAL)
         init_db(app)
-        # Talisman will force all requests to your REST API to use the https:// protocol.
-        # This is a good thing, except perhaps when testing (-> all tests will fail).
-        talisman.force_https = False
 
     @classmethod
     def tearDownClass(cls):
